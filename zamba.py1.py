803dca6cd3e898de2d96dd9edaa5fa3d263c808a
Ayush Nanda <ananda_be18@thapar.edu>
2021-02-16 20:21:50 +0530

    recognising users voice working

---
+++ zamba.py1.py
@@ -0,0 +1,56 @@
+import pyttsx3
+import datetime #for wish me
+import speech_recognition as sr #for taking input from user
+
+#Initialized voices to be used 
+engine =pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+#Speak function to be used by microsoft voices
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    '''uses speak function to greet user(assistant)'''
+    hour =int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+
+    else:
+        speak("Good Evening")
+
+    speak("Zambo Welcomes You! How may I help you ?")
+
+def askCommand():
+    '''using microphone to ask user for input'''
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.........")
+
+        r.pause_threshold= 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query =r.recognize_google(audio,language='en-in')
+        print(f"User said :{query}\n")
+
+    except Exception as e:
+       #print(e)
+
+        print("I couldn't understand. Please repeat..")
+        return "None"
+    return query
+
+    
+
+if __name__=="__main__":   #main function
+    wishMe();      #function calling
+    
+    query = askCommand().lower()
